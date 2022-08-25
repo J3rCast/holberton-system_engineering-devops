44606fd0013952a64ff056b4059c105dab7ec2ba
J3rCast <jerson.castro.r@gmail.com>
2022-08-25 13:46:33 -0500

    third task fixed

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,13 +1,14 @@
 # create a custom HTTP header response
 exec { 'append to a file':
   command => 'echo "frontend first_frontend
-    bind :80
-    default_backend web_servers
+	bind :80
+	default_backend web_servers
     
 backend web_servers
-    balance roundrobin
-    server web-01 54.227.49.160
-    server web-02 3.88.139.128" >> /etc/haproxy/haproxy.cfg'
+	balance roundrobin
+	server web-01 54.227.49.160
+	server web-02 3.88.139.128" >> /etc/haproxy/haproxy.cfg',
+  path    => '/bin'
 }
 exec { 'restart nginx':
   command => '/etc/init.d/nginx restart'
