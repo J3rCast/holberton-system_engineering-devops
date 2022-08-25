f41220a86f1139cd916e4d869920f9b1248e8149
J3rCast <jerson.castro.r@gmail.com>
2022-08-25 17:44:24 -0500

    third task added

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,11 +1,12 @@
 # create a custom HTTP header response
 package { 'nginx':
   ensure => installed,
-  name   => $nginx
+  name   => nginx
 }
 file { '/etc/nginx/sites-available/default':
   ensure => present,
-  source => '/home/ubuntu/new_default_1'
+  source => '/home/jerson/Holberton/holbertonschool-system_engineering-devops/0x0F-load_balancer/new_default_1'
+  #source => '/home/ubuntu/new_default_1'
 }
 exec { 'restart nginx':
   command => '/etc/init.d/nginx restart'
