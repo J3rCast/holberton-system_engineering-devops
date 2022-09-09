d66f57f4002ba80e815f51e28818c34146ea8a99
J3rCast <jerson.castro.r@gmail.com>
2022-09-09 07:30:42 -0500

    pycodestyle fix

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -7,22 +7,22 @@
 import requests
 from sys import argv
 
+if __name__ == "__main__":
+    emp_id = argv[1]
+    tasks_count = 0
+    tasks_title = []
 
-emp_id = argv[1]
-tasks_count = 0
-tasks_title = []
+    r_td = requests.get('https://jsonplaceholder.typicode.com/todos?userId='
+                        + emp_id)
+    r_usr = requests.get('https://jsonplaceholder.typicode.com/users/'
+                         + emp_id)
 
-r_td = requests.get('https://jsonplaceholder.typicode.com/todos?userId='
-                    + emp_id)
-r_usr = requests.get('https://jsonplaceholder.typicode.com/users/'
-                     + emp_id)
+    for tasks in r_td.json():
+        if tasks.get("completed"):
+            tasks_count += 1
+            tasks_title.append(tasks.get("title"))
 
-for tasks in r_td.json():
-    if tasks.get("completed"):
-        tasks_count += 1
-        tasks_title.append(tasks.get("title"))
-
-print("Employee {} is done with tasks({}/{}):".format(
-    r_usr.json().get("name"), tasks_count, len(r_td.json())))
-for i in tasks_title:
-    print("\t {}".format(i))
+    print("Employee {} is done with tasks({}/{}):".format(
+        r_usr.json().get("name"), tasks_count, len(r_td.json())))
+    for i in tasks_title:
+        print("\t {}".format(i))
